7feebb95c14f3aad1ee6f4a441462043f4031303
lordyabu <ay13@illinois.edu>
2023-11-13 20:05:23 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
 # Project Structure
-
+Check /notes for high level documentation notes
 ## Key Files
-
-- Bollinger Bands Analysis
-  - [bollingerNaive.py](/src/bollingerBands/bollingerNaive.py)
-  - [calculateBands.py](/src/bollingerBands/calculateBands.py)
-
-- Turtle Trading Strategy
-  - [turtleNaive.py](/src/turtles/turtleNaive.py)
-  - [calculateWindows.py](/src/turtles/calculateWindows.py)
-
-- Trade Analysis
-  - [getStats.py](/src/tradeAnalysis/getStats.py)
-  - [getGraphs.py](/src/tradeAnalysis/getGraphs.py)
-  - [graphBollingerTrades.py](/src/bollingerBands/analysis/graphBollingerTrades.py)
-  - [graphTurtleTrades.py](/src/turtles/analysis/graphTurtleTrades.py)
-
 - Data Examination and Main Execution
-  - [main.py](/src/main.py)
-  - [examine_data.py](/src/examine_data.py)
+  - [main.py](/src/main.py) - Runs the data preprocessing / trades - (Set flags for each operation)
+  - [examine_data.py](/src/examine_data.py) - Aggregates the Trade Analysis section below
+
+- Matlab and Major operations
+  - [getindividualstocks.m](/src/matlabExtraction/getindivudalstocks.m) - Converts Rosettaupdated to individual csvs
+  - [majorOperations.py](/src/helperFunctions/dataPreprocessing/majorOperations.py) - Applies major operations to dataset
 
 - OHLC Calculations
-  - [ohlcCalculations.py](/src/helperFunctions/dataPreprocessing/ohlcCalculations.py)
+  - [ohlcCalculations.py](/src/helperFunctions/dataPreprocessing/ohlcCalculations.py) - Calculates OHLC from 5min return data
+
+
+- Bollinger Bands Analysis
+  - [bollingerNaive.py](/src/bollingerBands/bollingerNaive.py) - Trader class for bollinger
+  - [calculateBands.py](/src/bollingerBands/calculateBands.py) - Calculates / Saves bands for bollinger
+
+- Turtle Trading Strategy
+  - [turtleNaive.py](/src/turtles/turtleNaive.py) - Trader class for turtles
+  - [calculateWindows.py](/src/turtles/calculateWindows.py) - Calculates / Saves windows for turtle
+
+- Trade Analysis
+  - [getStats.py](/src/tradeAnalysis/getStats.py) - Get basic trade stats
+  - [getGraphs.py](/src/tradeAnalysis/getGraphs.py) - Get basic trade graphs
+  - [graphBollingerTrades.py](/src/bollingerBands/analysis/graphBollingerTrades.py) - Get detailed graph with bollinger trade points
+  - [graphTurtleTrades.py](/src/turtles/analysis/graphTurtleTrades.py) - Get detailed graph with turtle trade points
+
+    
--- src/main.py
+++ src/main.py
@@ -1,62 +1,70 @@
 from tqdm import tqdm
 import os
 from src.config import DATA_DIR, BOLLINGER_DATA_DIR, TURTLE_DATA_DIR, OHLC_DATA_DIR
-# 0. Decide what operations to run. If this is fresh run, I suggest having all set to True
+
+# Suppressing warnings for cleaner output // Pandas DF slicing
+import warnings
+warnings.filterwarnings('ignore')
+
+# ==========================
+# Configuration and Settings (Make sure to configure the operations you turn to True below)
+# ==========================
+
+# Deciding which operations to run (set to True to execute)
 do_major_ops = False
 do_ohlc_calculations = False
 select_date_range = False
 do_band_calculation = False
-run_bollinger_trades = True
+run_bollinger_trades = False
 do_window_calculation = False
-run_turtle_trades = True
+run_turtle_trades = False
 
-# 1. Assuming you have run the provided MatLab script, Have data directory in your Documents Folder Structured like this.
-# Only dataDailyTwoCol and dataFiveMin need to have data in currently.
+# ======================================================
+# Directory Structure (Ensure this matches your setup) - (If you want to change names go to config.py(don't change helperData or tradeData))
+# ======================================================
 
 """
 CrspData
 │
-├── bollingerData       - Description of 'bollingerData' contents
-├── turtleData          - Description of 'dataDailyTwoCol' contents
-├── dataFiveMin         - Description of 'dataFiveMin' contents
-├── helperData          - Contains helper files and data(valid stock names)
-├── priceData           - Raw price data
+├── bollingerData       - Contains data for Bollinger Bands analysis
+├── turtleData          - Contains data for Turtle strategy analysis
+├── dataFiveMin         - Contains 5-minute interval data
+├── helperData          - Contains helper files and data (valid stock names)
+├── priceDataRAW        - Raw price data
+├── priceDataMO         - Price data POST major op
 ├── priceDataOHLC       - Price data with Open-High-Low-Close (OHLC)
-├── priceDataTest       - Test data for price information
-├── testData            - General test data used across the project
 └── tradeData           - Trade-related data
-
-Note: Replace the descriptions with more specific information about what each directory contains.
 """
 
-# 1. Apply your major operations to the dataset. For more what details on what a major op is go to ./notes/DataPreprocessing.txt.txt
+# =======================================
+# Major Operations and Data Preprocessing
+# =======================================
+
 if do_major_ops:
-    from src.tests.test_price_calculations import TestMajorOp
     from src.helperFunctions.dataPreprocessing.majorOperations import major_operations
 
+    # Configuration for major operations
     positive_major_op = False
     NaNs_major_op = True
     save_to_major_op_log = True
 
-    # Major Operations with Progress Bar
+    # Performing major operations with progress tracking
     print("Performing major operations...")
-    with tqdm(total=2, desc="Major Operations") as pbar:
-        major_operations(positive=positive_major_op, NaNs=NaNs_major_op, save_to_log=save_to_major_op_log)
-        pbar.update(1)
+    major_operations(positive=positive_major_op, NaNs=NaNs_major_op, save_to_log=save_to_major_op_log)
 
-        test_major_op = TestMajorOp()
-        test_major_op.set_test_flags(NaNs_major_op, positive_major_op)
 
-        test_major_op.test_NaNs_major_op()
-        pbar.update(1)
-        test_major_op.test_positive_major_op()
-
+# ====================================
+# Open-High-Low-Close (OHLC) Calculations
+# ====================================
 
 if do_ohlc_calculations:
     from src.helperFunctions.dataPreprocessing.getOHLCfrom5MinRet import get_ohlc
     print("Performing OHLC operations...")
     get_ohlc()
 
+# =============================
+# Date Range Selection for Data
+# =============================
 
 if select_date_range:
     from src.tests.test_price_calculations import TestMajorOp
@@ -69,30 +77,38 @@ if select_date_range:
     print("Performing timespan operations...")
     save_stock_filenames_in_timespan_daily(start=start, end=end, exclude_nan=exclude_nan, exclude_negative=exclude_negative)
 
+    # Test Data Set Validity Now
     test_major_op = TestMajorOp()
-    test_major_op.set_test_flags(True, True)
-
+    test_major_op.set_test_flags(test_NaNs=exclude_nan, test_positives=exclude_negative)
     test_major_op.test_NaNs_major_op()
     test_major_op.test_positive_major_op()
 
 
-
+# ==========================
+# Bollinger Bands Calculation
+# ==========================
 
 if do_band_calculation:
-    print("running band calculations")
+    print("Running Bollinger Bands calculations...")
     from src.bollingerBands.calculateBands import BollingerBands
 
     directory = OHLC_DATA_DIR
     bollinger = BollingerBands(data_directory=directory)
     bollinger.calculate_all_bands()
 
+# =======================
+# Running Bollinger Trades
+# =======================
 
 if run_bollinger_trades:
-    print("running bollinger")
+    print("Running Bollinger Band trades...")
     from src.bollingerBands.runBands.runBollingerAll import run_all_bollinger_trades
 
-    run_all_bollinger_trades('test10bollinger')
+    run_all_bollinger_trades('test1bollinger')
 
+# ============================
+# Turtle Strategy Calculations
+# ============================
 
 if do_window_calculation:
     from src.turtles.calculateWindows import TurtleWindows
@@ -101,6 +117,9 @@ if do_window_calculation:
     turtle_windows = TurtleWindows(data_directory=directory)
     turtle_windows.calculate_all_windows()
 
+# ======================
+# Running Turtle Trades
+# ======================
 
 if run_turtle_trades:
     print("runningturtles")
