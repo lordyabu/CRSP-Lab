d2f983d9fa9bad4deeaf836d15744fa1fe645119
lordyabu <ay13@illinois.edu>
2023-11-14 14:49:33 -0600

    timespan

--- src/examine_data.py
+++ src/examine_data.py
@@ -13,7 +13,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # Strategies: bollinger_naive_dynamic_sl, turtle_naive
-# trades_df = extract_trades(strategy='turtle_naive', sort_by='EndDate')
+trades_df = extract_trades(strategy='turtle_naive', sort_by='EndDate')
 start_date = '2010-01-04'
 end_date = '2020-12-31'
 
@@ -22,7 +22,7 @@ end_date = '2020-12-31'
 
 # Plot various graphs for the trading strategy
 # Uncomment the line below to execute
-# plot_everything(trades_df, start_date, end_date)
+plot_everything(trades_df, start_date, end_date)
 
 # Get and print trade statistics
 # Uncomment the line below to execute
@@ -48,5 +48,5 @@ start_date_window_turtle = '2018-10-11'
 end_date_window_turtle = '2020-12-31'
 # Visualize Turtle trades for a specific window
 # Uncomment the line below to execute
-graph_window_turtle(start_date_window_turtle, end_date_window_turtle, 'AAPL', 'turtle_naive')
+# graph_window_turtle(start_date_window_turtle, end_date_window_turtle, 'AAPL', 'turtle_naive')
 # ---------------------------------
